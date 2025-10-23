fa399af57cbbc476f6732c4f7dd98ef11f4299af
timchr42 <timjannis03@gmail.com>
2025-10-23 10:00:24 +0200

    bug fix introduced in last commits: Dont allow private wildcard tokens to be read, otherwise tracking again possible by library that lives inside app with same rights

--- netwerk/cookie/CookieService.cpp
+++ netwerk/cookie/CookieService.cpp
@@ -677,7 +677,6 @@ CookieService::SetCookieStringFromHttp(nsIURI* aHostURI,
     case byetrack::ByetrackCookieAction::CaptureWildcard: {
       decision.token->SetCookieName(CHIPSCookieName);
       decision.token->SetCookieValue(CHIPSCookieValue);
-      decision.token->SetAccessRights(byetrack::AccessRights::READ_WRITE);  // Update token to be read and writable for wildcard private ones
       printf_stderr("Byetrack (CookieService) Wildcard token (%s) updated with cookie name and value; try staging for return\n", decision.token->ToCharArray());
 
       return StageTokenForReturn(aChannel, decision.token, baseDomain, CHIPSCookieName, CHIPSCookieValue, aCookieHeader);
